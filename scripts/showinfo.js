9274c35858206baadeed413d4c38990e37f0c387
cvalencia1991 <cesar4a6z@gmail.com>
2022-09-14 23:47:43 -0500

    Edit About Page Javascript

--- scripts/showinfo.js
+++ scripts/showinfo.js
@@ -10,38 +10,4 @@ function closemenu() {
 }
 document.getElementById('closebutton').addEventListener('click', closemenu);
 
-/* button Showmore */
-/* function showmore(){
-    const button = document.getElementById('showmore')
-    show2 = document.getElementById('show2')
-
-    //cards
-    const card1 = document.getElementById('card1')
-    const card2 = document.getElementById('card2')
-    const card3 = document.getElementById('card3')
-    const card4 = document.getElementById('card4')
-
-    button.style.display='none'  //hide button
-
-    //show2.style.display=''
-    show2.style.cssText=`
-        display: block !important
-    `;
-
-    card1.style.cssText=`
-        display: flex !important
-    `;
-
-    card2.style.cssText=`
-        display: flex !important
-    `;
-
-    card3.style.cssText=`
-        display: flex !important
-    `;
-
-    card4.style.cssText=`
-        display: flex !important
-    `;
-
-} */
+/* About Page */
--- scripts/showinfo2.js
+++ scripts/showinfo2.js
@@ -1,47 +1,11 @@
-function deploymenu() {
-  const menubar = document.getElementById('menubar');
-  menubar.style.display = 'block';
+function deploymenu2() {
+  const menubar = document.getElementById('menubar2');
+  menubar.style.display = 'flex';
 }
-document.getElementById('deploy-menu').addEventListener('click', deploymenu);
+document.getElementById('deploy-menu2').addEventListener('click', deploymenu2);
 
-function closemenu() {
-  const menubar = document.getElementById('menubar');
+function closemenu2() {
+  const menubar = document.getElementById('menubar2');
   menubar.style.display = 'none';
 }
-document.getElementById('closebutton').addEventListener('click', closemenu);
-
-/* button Showmore */
-/* function showmore(){
-    const button = document.getElementById('showmore')
-    show2 = document.getElementById('show2')
-
-    //cards
-    const card1 = document.getElementById('card1')
-    const card2 = document.getElementById('card2')
-    const card3 = document.getElementById('card3')
-    const card4 = document.getElementById('card4')
-
-    button.style.display='none'  //hide button
-
-    //show2.style.display=''
-    show2.style.cssText=`
-        display: block !important
-    `;
-
-    card1.style.cssText=`
-        display: flex !important
-    `;
-
-    card2.style.cssText=`
-        display: flex !important
-    `;
-
-    card3.style.cssText=`
-        display: flex !important
-    `;
-
-    card4.style.cssText=`
-        display: flex !important
-    `;
-
-} */
+document.getElementById('closebutton2').addEventListener('click', closemenu2);
