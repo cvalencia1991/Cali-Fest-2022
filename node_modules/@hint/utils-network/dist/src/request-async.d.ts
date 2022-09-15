6b8f2128ed4a35e689e2b0bd0ffdf7792fdcd127
Cesar Valencia <107327344+cvalencia1991@users.noreply.github.com>
2022-09-15 01:04:41 -0500

    Delete node_modules directory

--- node_modules/@hint/utils-network/dist/src/request-async.d.ts
+++
@@ -1,8 +0,0 @@
-import { RequestInit } from 'node-fetch';
-interface IRequestOptions extends RequestInit {
-    rejectUnauthorized?: boolean;
-    strictSSL?: boolean;
-}
-export declare const requestAsync: (url: string, options?: IRequestOptions) => Promise<any>;
-export {};
-//# sourceMappingURL=request-async.d.ts.map
--- node_modules/@hint/utils-types/dist/src/fix.d.ts
+++
@@ -1,6 +0,0 @@
-import { ProblemLocation } from './problem-location';
-export declare type CodeFix = {
-    location: ProblemLocation;
-    text: string;
-};
-//# sourceMappingURL=fix.d.ts.map
